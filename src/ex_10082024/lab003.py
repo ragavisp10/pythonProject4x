3b0b8126880ea0c59e0f1aa6e8fea22ccd9765f5
ragavisp10 <ragavisp1006@gmail.com>
2024-08-12 19:23:58 +0530

    changes added

---
+++ src/ex_10082024/lab003.py
@@ -0,0 +1,9 @@
+# Python program
+# sep = ' ' - How do you want to seperate the arguements (by default space is seperator)
+# end = "\n" - in end what do you want
+print("Hello","World","Hiii",22,sep='-')
+print("Hello","World","Hiii",22,sep='/')
+print("Hello","World","Hiii",22,end="-")
+print("Hello!")
+
+
---
+++ src/ex_10082024/lab004.py
@@ -0,0 +1,4 @@
+# program to find maximum between integers
+print(max(10,6))
+print(max(100,23.78,100.1,47))
+
