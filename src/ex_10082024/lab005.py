a0a981fc23b9fe3cea3c631bf840ff7c10d1c4eb
ragavisp10 <ragavisp1006@gmail.com>
2024-08-13 19:30:28 +0530

    changes added

---
+++ src/ex_10082024/lab005.py
@@ -0,0 +1,13 @@
+# Keywords and identifiers
+# import keyword
+# print(keyword.kwlist)
+int=63
+pi=3.14
+name="Ragavi"
+isFemale=True
+print(type(pi))
+print(type(name))
+print(type(isFemale))
+complex_no=1+4j
+print(complex_no.real)
+print(complex_no.imag)
